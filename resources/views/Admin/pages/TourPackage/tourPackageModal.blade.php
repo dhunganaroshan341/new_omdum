c8bf0d6d57caa59561c0c0251486213874f6baed
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-25 22:40:42 +0545

    Auto commit changes

--- resources/views/Admin/pages/TourPackage/tourPackageModal.blade.php
+++ resources/views/Admin/pages/TourPackage/tourPackageModal.blade.php
@@ -96,26 +96,40 @@
 
                         <div class="col-12">
                             <label class="form-label">Tour Package Tags</label>
-                            <div class="d-flex flex-wrap gap-4 align-items-center py-2">
-                                <!-- Top Deal Toggle -->
-                                <div class="form-check form-switch">
-                                    <input type="hidden" name="top_deal" value="0">
-                                    <input class="form-check-input" type="checkbox" id="topDealSwitch" name="top_deal"
-                                        value="1">
-                                    <label class="form-check-label ms-2" for="topDealSwitch">Top Deal</label>
+                            <div class="row">
+
+                                <!-- Top Deal -->
+                                <div class="col-md-6">
+                                    <div class="row align-items-center">
+                                        <div class="col-2">
+                                            <input type="hidden" name="top_deal" value="0">
+                                            <input type="checkbox" id="topDeal" name="top_deal" value="1"
+                                                class="form-check-input">
+                                        </div>
+                                        <div class="col-10">
+                                            <label class="form-check-label" for="topDeal">Top Deal</label>
+                                        </div>
+                                    </div>
                                 </div>
 
-                                <!-- Favourite Destination Toggle -->
-                                <div class="form-check form-switch">
-                                    <input type="hidden" name="favourite_destination" value="0">
-                                    <input class="form-check-input" type="checkbox" id="favouriteDestinationSwitch"
-                                        name="favourite_destination" value="1">
-                                    <label class="form-check-label ms-2" for="favouriteDestinationSwitch">
-                                        Favourite Destination
-                                    </label>
+                                <!-- Favourite Destination -->
+                                <div class="col-md-6">
+                                    <div class="row align-items-center">
+                                        <div class="col-2">
+                                            <input type="hidden" name="favourite_destination" value="0">
+                                            <input type="checkbox" id="favouriteDestination"
+                                                name="favourite_destination" value="1" class="form-check-input">
+                                        </div>
+                                        <div class="col-10">
+                                            <label class="form-check-label" for="favouriteDestination">Favourite
+                                                Destination</label>
+                                        </div>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
+
 
                         <div class="col-12">
                             <label>Short Description</label>
