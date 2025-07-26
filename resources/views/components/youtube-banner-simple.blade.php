ad44b05af173832fe2da37dddc1da1da5011bfd7
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-26 20:35:58 +0545

    Auto commit changes

--- resources/views/components/youtube-banner-simple.blade.php
+++ resources/views/components/youtube-banner-simple.blade.php
@@ -1,4 +1,4 @@
-<iframe width="560" height="315" src="https://www.youtube.com/embed/-451q_49OnY?si=t-35xNp0s4lXS1ko"
+<iframe width="1300px" height="680px" src="https://www.youtube.com/embed/-451q_49OnY?si=t-35xNp0s4lXS1ko"
     title="YouTube video player" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
