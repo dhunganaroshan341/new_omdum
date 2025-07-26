baf25aa0850a327a12114ae357da67786efda774
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-26 14:25:45 +0545

    Auto commit changes

--- app/Http/Controllers/UserFrontendController.php
+++ app/Http/Controllers/UserFrontendController.php
@@ -32,7 +32,7 @@ class UserFrontendController extends Controller
     $homeslides = HomeSlide::where('status', 'Active')->get();
     $testimonials = Testimonial::where('status', 'Active')->get();
     $destinations = Destination::where('status', 'Active')->get();
-    $topDestinations = Destination::where('status', 'Active')->where('top_destination',1)->get();
+    $topDestinations = Destination::where('status', 'Active')->where('top_deal',1)->get();
     $favDestinations = Destination::where('status', 'Active')->where('favourite_destination',1)->get();
 
     $clients = \App\Models\Client::with('albums')->get();
