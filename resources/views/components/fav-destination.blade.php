9322ff9707fce259cc0e80ebdd0822faf8f20f78
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-26 17:02:00 +0545

    Auto commit changes

--- resources/views/components/fav-destination.blade.php
+++ resources/views/components/fav-destination.blade.php
@@ -1,31 +1,79 @@
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-    @foreach ($favouriteDestinationsByCountry as $country => $packages)
-        <li class="nav-item" role="presentation">
-            <button class="nav-link {{ $loop->first ? 'active' : '' }}" id="tab-{{ Str::slug($country) }}-tab"
-                data-bs-toggle="pill" data-bs-target="#tab-{{ Str::slug($country) }}" type="button" role="tab">
-                {{ $country }}
-            </button>
-        </li>
-    @endforeach
-</ul>
+<section class="trending destination pb-6 bg-grey pt-9">
+    <div class="container">
+        <div class="section-title text-center mb-5 pb-2 w-50 mx-auto">
+            <h2 class="m-0">Choose Your <span>Favourite Destination</span></h2>
+        </div>
 
-<div class="tab-content" id="pills-tabContent">
-    @foreach ($favouriteDestinationsByCountry as $country => $packages)
-        <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="tab-{{ Str::slug($country) }}"
-            role="tabpanel">
-            <div class="row">
-                @foreach ($packages as $package)
-                    <div class="col-md-4 mb-3">
-                        <div class="card h-100">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $package->title }}</h5>
-                                <p class="card-text">{{ $package->description }}</p>
-                                {{-- Add more details like image, price, etc. --}}
-                            </div>
+        <div class="trend-box">
+            {{-- Nav Tabs --}}
+            <div class="price-navtab text-center mb-4">
+                <ul class="nav nav-tabs">
+                    @foreach ($favouriteDestinationsByCountry as $country => $packages)
+                        <li class="nav-item">
+                            <a class="nav-link {{ $loop->first ? 'active' : '' }}" data-bs-toggle="tab"
+                                href="#{{ Str::slug($country) }}">
+                                {{ $country }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            {{-- Tab Contents --}}
+            <div class="tab-content">
+                @foreach ($favouriteDestinationsByCountry as $country => $packages)
+                    <div id="{{ Str::slug($country) }}" class="tab-pane fade {{ $loop->first ? 'show active' : '' }}">
+                        <div class="row">
+                            @foreach ($packages as $package)
+                                <div class="col-lg-4 col-md-6 mb-4">
+                                    <div class="trend-item">
+                                        <div class="trend-image">
+                                            <img src="{{ asset($package->images[0] ?? 'template/yatri_world/main-file/images/default.jpg') }}"
+                                                alt="{{ $package->title }}">
+                                        </div>
+                                        <div class="trend-content-main">
+                                            <div class="trend-content">
+                                                <h4>
+                                                    <a href="{{ route('packages.show', $package->slug) }}">
+                                                        {{ $package->title }}
+                                                    </a>
+                                                </h4>
+                                                <p class="mb-0">
+                                                    <i class="fa fa-eye me-1"></i>
+                                                    {{ $package->visiting_places ?? '0' }} Visiting Places
+                                                    <i class="fa fa-map-marker me-1 ms-3"></i>
+                                                    {{ $country }}
+                                                </p>
+                                            </div>
+                                            <div class="trend-last-main">
+                                                <p class="mb-0 trend-para">
+                                                    {{ Str::limit($package->short_description, 100) }}
+                                                </p>
+                                                <div
+                                                    class="trend-last d-flex align-items-center justify-content-center">
+                                                    <a href="{{ route('packages.show', $package->slug) }}"
+                                                        class="book-now-a mb-0 white d-flex align-items-center">
+                                                        <img src="{{ asset('template/yatri_world/main-file/images/india.jpg') }}"
+                                                            class="d-author me-2" alt="">
+                                                        Book Now
+                                                    </a>
+                                                    <div class="trend-price">
+                                                        <p class="price white mb-0">
+                                                            <a href="{{ route('packages.show', $package->slug) }}">
+                                                                <i class="fa fa-arrow-right white ps-1"></i>
+                                                            </a>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 @endforeach
             </div>
         </div>
-    @endforeach
-</div>
+    </div>
+</section>
