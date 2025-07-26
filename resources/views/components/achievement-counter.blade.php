e6f0b379586d55f125fac7ef73560d61476cf90e
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-26 14:49:06 +0545

    Auto commit changes

--- resources/views/components/achievement-counter.blade.php
+++ resources/views/components/achievement-counter.blade.php
@@ -3,12 +3,11 @@
     <div class="container">
         <div class="counter text-center">
             <div class="row">
-                @dd($achievements)
                 @foreach ($achievements as $achievement)
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="counter-item bg-lgrey">
-                            <i class="fa {{ $achievement->icon }} white bg-navy mb-1"></i>
-                            <h3 class="value mb-0 navy">{{ $achievement->value }}</h3>
+                            <i class="{{ $achievement->icon_class }} white bg-navy mb-1"></i>
+                            <h3 class="value mb-0 navy">{{ $achievement->count }}</h3>
                             <h4 class="m-0">{{ $achievement->title }}</h4>
                         </div>
                     </div>
