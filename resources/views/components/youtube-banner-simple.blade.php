e773494980153e835aa25c6ff06b71ab92067ba3
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-27 12:23:21 +0545

    Auto commit changes

--- resources/views/components/youtube-banner-simple.blade.php
+++ resources/views/components/youtube-banner-simple.blade.php
@@ -1,4 +1,4 @@
-<iframe width="1300px" height="680px"
+<iframe width="1920px" height="680px"
     src="https://www.youtube.com/embed/-451q_49OnY?autoplay=1&mute=1&loop=1&playlist=-451q_49OnY"
     title="YouTube video player" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
