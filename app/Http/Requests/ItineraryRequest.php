f98acd4dc7d493551ef206f6c9c543f56d2335bf
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-27 15:30:21 +0545

    Auto commit changes

--- app/Http/Requests/ItineraryRequest.php
+++ app/Http/Requests/ItineraryRequest.php
@@ -2,6 +2,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ItineraryRequest extends FormRequest
 {
@@ -18,7 +19,15 @@ class ItineraryRequest extends FormRequest
             'day_number'      => 'nullable|integer|min:1|max:365',
             'status'          => 'nullable|in:Active,Inactive',
             'title'           => 'nullable|string|max:255',
-            'order'           => 'nullable|integer|unique:itineraries,order,' . $this->id,
+             'order'           => [
+            'nullable',
+            'integer',
+            Rule::unique('itineraries')
+                ->where(function ($query) {
+                    return $query->where('tour_package_id', $this->tour_package_id);
+                })
+                ->ignore($this->id) // for update case
+        ],
             'description'     => 'required|string|min:10',
         ];
     }
