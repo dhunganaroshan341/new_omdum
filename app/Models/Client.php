b4492cdf753783601c9d0216a4a675a00a53bbeb
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-28 16:40:01 +0545

    Auto commit changes

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -9,10 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends BaseModel
 {
     use HasFactory;
-     use HasUploadUrl;
-    // If your column is named 'image_path' (default), no need to set this
-    protected $uploadPathColumn = 'image';
-    protected $appends = ['image_url'];
+    //  use HasUploadUrl;
+    // // If your column is named 'image_path' (default), no need to set this
+    // protected $uploadPathColumn = 'image';
+    // protected $appends = ['image_url'];
 
     protected $fillable=['name','email','address','contact','image','description'];
     public function albums()
