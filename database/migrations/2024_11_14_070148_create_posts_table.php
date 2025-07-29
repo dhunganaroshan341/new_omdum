898069b767f91597e3957ef10000da566698a0d5
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-29 12:10:00 +0545

    Auto commit changes

--- database/migrations/2024_11_14_070148_create_posts_table.php
+++ database/migrations/2024_11_14_070148_create_posts_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->unsignedBigInteger('created_by');
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->enum('status',['Active','Inactive'])->default('Active');
-            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade');
             $table->foreign('created_by')->references('id')->on('users')->onUpdate('cascade');
             $table->foreign('updated_by')->references('id')->on('users')->onUpdate('cascade');
             $table->timestamps();
