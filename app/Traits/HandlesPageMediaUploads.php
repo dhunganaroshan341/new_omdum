9daf6847080ebea0419e31bbe14a9abb55184bc0
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-30 17:24:53 +0545

    Auto commit changes

--- app/Traits/HandlesPageMediaUploads.php
+++ app/Traits/HandlesPageMediaUploads.php
@@ -1,20 +1,14 @@
 <?php
+
 namespace App\Traits;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 trait HandlesPageMediaUploads
 {
     /**
      * Handle single media uploads.
-     * Deletes old files if exists before storing new ones.
-     *
-     * @param Request $request
-     * @param array $fields
-     * @param array $existingData Current existing data (to delete old files)
-     * @param string $folder
-     * @return array
+     * Deletes old files from public path and stores new ones.
      */
     public function handleSingleMediaUploads(Request $request, array $fields, array $existingData = [], $folder = 'uploads/pages')
     {
@@ -23,11 +17,15 @@ trait HandlesPageMediaUploads
         foreach ($fields as $field) {
             if ($request->hasFile($field)) {
                 // Delete old file if exists
-                if (!empty($existingData[$field]) && Storage::disk('public')->exists($existingData[$field])) {
-                    Storage::disk('public')->delete($existingData[$field]);
+                if (!empty($existingData[$field]) && file_exists(public_path($existingData[$field]))) {
+                    @unlink(public_path($existingData[$field]));
                 }
+
                 // Store new file
-                $mediaData[$field] = $request->file($field)->store($folder, 'public');
+                $file = $request->file($field);
+                $fileName = uniqid() . '_' . $file->getClientOriginalName();
+                $file->move(public_path($folder), $fileName);
+                $mediaData[$field] = "$folder/$fileName";
             }
         }
 
@@ -36,19 +34,16 @@ trait HandlesPageMediaUploads
 
     /**
      * Handle gallery uploads (multiple images).
-     *
-     * @param Request $request
-     * @param string $field
-     * @param string $folder
-     * @return array
      */
     public function handleGalleryUploads(Request $request, $field = 'gallery_images', $folder = 'uploads/pages/gallery')
     {
         $paths = [];
 
         if ($request->hasFile($field)) {
-            foreach ($request->file($field) as $image) {
-                $paths[] = $image->store($folder, 'public');
+            foreach ($request->file($field) as $file) {
+                $fileName = uniqid() . '_' . $file->getClientOriginalName();
+                $file->move(public_path($folder), $fileName);
+                $paths[] = "$folder/$fileName";
             }
         }
 
