23d6745fa5958855206fcae634079726a481d1c5
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-30 16:09:15 +0545

    Auto commit changes

--- resources/views/Admin/pages/pages/pages.blade.php
+++ resources/views/Admin/pages/pages/pages.blade.php
@@ -1,30 +1,27 @@
 @extends('Admin.layout.master')
 
-@section('content')
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
-        Launch demo modal
-    </button>
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
+@section('content')
+    <div class="container-fluid">
+        <button class="btn btn-primary mb-4 mt-4">Pages</button>
+
+
+
+        <div class="table-responsive">
+            <table class="table table-striped" id="show-page-data">
+                <thead>
+                    <tr>
+                        <th scope="col">S.N</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+
+
+            </table>
         </div>
+
     </div>
 @endsection
