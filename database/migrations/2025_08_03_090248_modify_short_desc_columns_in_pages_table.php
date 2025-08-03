8fd41e02f8b96f12cc96b15d6a3fbf1168f3147e
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-03 15:03:54 +0545

    Auto commit changes

--- database/migrations/2025_08_03_090248_modify_short_desc_columns_in_pages_table.php
+++ database/migrations/2025_08_03_090248_modify_short_desc_columns_in_pages_table.php
@@ -8,16 +8,16 @@ class ModifyShortDescColumnsInPagesTable extends Migration
     public function up()
     {
         Schema::table('pages', function (Blueprint $table) {
-            $table->text('short_desc_1')->change();
-            $table->text('short_desc_2')->change();
+            $table->text('short_desc1')->change();
+            $table->text('short_desc2')->change();
         });
     }
 
     public function down()
     {
         Schema::table('pages', function (Blueprint $table) {
-            $table->string('short_desc_1', 255)->change();
-            $table->string('short_desc_2', 255)->change();
+            $table->string('short_desc1', 255)->change();
+            $table->string('short_desc2', 255)->change();
         });
     }
 }
