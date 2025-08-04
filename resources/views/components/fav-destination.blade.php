1e7d2f9ad062991eb7276408d552059e233560a3
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-04 14:36:41 +0545

    Auto commit changes

--- resources/views/components/fav-destination.blade.php
+++ resources/views/components/fav-destination.blade.php
@@ -5,17 +5,18 @@
         </div>
 
         <div class="trend-box">
-
             {{-- Nav Tabs --}}
             <div class="price-navtab text-center mb-4">
                 <ul class="nav nav-tabs">
                     @foreach ($favouriteDestinationsByCountry as $country)
-                        <li class="nav-item">
-                            <a class="nav-link {{ $loop->first ? 'active' : '' }}" data-bs-toggle="tab"
-                                href="#{{ \Illuminate\Support\Str::slug($country->name) }}">
-                                {{ $country->name }}
-                            </a>
-                        </li>
+                        @if ($country)
+                            <li class="nav-item">
+                                <a class="nav-link {{ $loop->first ? 'active' : '' }}" data-bs-toggle="tab"
+                                    href="#{{ \Illuminate\Support\Str::slug($country->name) }}">
+                                    {{ $country->name }}
+                                </a>
+                            </li>
+                        @endif
                     @endforeach
                 </ul>
             </div>
@@ -23,68 +24,69 @@
             {{-- Tab Contents --}}
             <div class="tab-content">
                 @foreach ($favouriteDestinationsByCountry as $country)
-                    <div id="{{ \Illuminate\Support\Str::slug($country->name) }}"
-                        class="tab-pane fade {{ $loop->first ? 'show active' : '' }}">
-                        <div class="row">
-                            @foreach ($country->tourPackages as $package)
-                                @php
-                                    $slug = $package->slug ?? null;
-                                    $routeUrl = $slug ? route('packages.show', $slug) : '#';
+                    @if ($country)
+                        <div id="{{ \Illuminate\Support\Str::slug($country->name) }}"
+                            class="tab-pane fade {{ $loop->first ? 'show active' : '' }}">
+                            <div class="row">
+                                @foreach ($country->tourPackages as $package)
+                                    @php
+                                        $slug = $package->slug ?? null;
+                                        $routeUrl = $slug ? route('packages.show', $slug) : '#';
 
-                                    // Use accessor - image_path already includes full asset() URL
-                                    $firstImage = $package->images->first();
-                                    $imageUrl = $firstImage
-                                        ? $firstImage->image_path
-                                        : asset('template/yatri_world/main-file/images/everest.jpg');
-                                @endphp
-                                <div class="col-lg-4 col-md-6 mb-4">
-                                    <div class="trend-item">
-                                        <div class="trend-image">
-                                            <img src="{{ $imageUrl }}"
-                                                alt="{{ $package->title ?? 'Package Image' }}">
-                                        </div>
-                                        <div class="trend-content-main">
-                                            <div class="trend-content">
-                                                <h4>
-                                                    <a href="{{ $routeUrl }}">
-                                                        {{ $package->title ?? 'Untitled Package' }}
-                                                    </a>
-                                                </h4>
-                                                <p class="mb-0">
-                                                    <i class="fa fa-map-marker me-1 ms-3"></i>
-                                                    {{ $country->name ?? 'Nepal' }}
-                                                </p>
+                                        // Safely get first image using accessor
+                                        $firstImage = $package->images?->first();
+                                        $imageUrl = $firstImage
+                                            ? $firstImage->image_path
+                                            : asset('template/yatri_world/main-file/images/everest.jpg');
+                                    @endphp
+                                    <div class="col-lg-4 col-md-6 mb-4">
+                                        <div class="trend-item">
+                                            <div class="trend-image">
+                                                <img src="{{ $imageUrl }}"
+                                                    alt="{{ $package->title ?? 'Package Image' }}">
                                             </div>
-                                            <div class="trend-last-main">
-                                                <p class="mb-0 trend-para">
-                                                    {{ \Illuminate\Support\Str::limit($package->short_description ?? '', 100) }}
-                                                </p>
-                                                <div
-                                                    class="trend-last d-flex align-items-center justify-content-center">
-                                                    <a href="{{ $routeUrl }}"
-                                                        class="book-now-a mb-0 white d-flex align-items-center">
-                                                        <img src="{{ asset('template/yatri_world/main-file/images/india.jpg') }}"
-                                                            class="d-author me-2" alt="Book Now Icon">
-                                                        Book Now
-                                                    </a>
-                                                    <div class="trend-price">
-                                                        <p class="price white mb-0">
-                                                            <a href="{{ $routeUrl }}">
-                                                                <i class="fa fa-arrow-right white ps-1"></i>
-                                                            </a>
-                                                        </p>
+                                            <div class="trend-content-main">
+                                                <div class="trend-content">
+                                                    <h4>
+                                                        <a href="{{ $routeUrl }}">
+                                                            {{ $package->title ?? 'Untitled Package' }}
+                                                        </a>
+                                                    </h4>
+                                                    <p class="mb-0">
+                                                        <i class="fa fa-map-marker me-1 ms-3"></i>
+                                                        {{ $country->name ?? 'Nepal' }}
+                                                    </p>
+                                                </div>
+                                                <div class="trend-last-main">
+                                                    <p class="mb-0 trend-para">
+                                                        {{ \Illuminate\Support\Str::limit($package->short_description ?? '', 100) }}
+                                                    </p>
+                                                    <div
+                                                        class="trend-last d-flex align-items-center justify-content-center">
+                                                        <a href="{{ $routeUrl }}"
+                                                            class="book-now-a mb-0 white d-flex align-items-center">
+                                                            <img src="{{ asset('template/yatri_world/main-file/images/india.jpg') }}"
+                                                                class="d-author me-2" alt="Book Now Icon">
+                                                            Book Now
+                                                        </a>
+                                                        <div class="trend-price">
+                                                            <p class="price white mb-0">
+                                                                <a href="{{ $routeUrl }}">
+                                                                    <i class="fa fa-arrow-right white ps-1"></i>
+                                                                </a>
+                                                            </p>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            @endforeach
+                                @endforeach
+                            </div>
                         </div>
-                    </div>
+                    @endif
                 @endforeach
             </div>
-
         </div>
     </div>
 </section>
