241601153ba135da2f0c3bf60e00b893e8020f02
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-06 13:46:47 +0545

    Auto commit changes

--- resources/views/frontend/gallery-media.blade.php
+++ resources/views/frontend/gallery-media.blade.php
@@ -3,15 +3,16 @@
 @section('content')
     <!-- BreadCrumb Starts -->
     <section class="breadcrumb-main"
-        style="background-image: url('{{ asset('template/yatri_world/main-file/images/bhutan.jpg') }}');">
+        style="background-image: url('{{ $pageBanner->image ?? asset('template/yatri_world/main-file/images/bhutan.jpg') }}');">
         <div class="breadcrumb-outer pt-10 pb-4">
             <div class="container">
                 <div class="breadcrumb-content d-md-flex align-items-center pt-10">
-                    <h2 class="mb-0">Omundum Trek Album</h2>
+                    <h2 class="mb-0">{{ $galleryAlbum->title ?? 'Album' }}</h2>
                     <nav aria-label="breadcrumb">
                         <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li aria-current="page" class="breadcrumb-item active">Gallery</li>
+                            <li class="breadcrumb-item"><a href="{{ route('index') }}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('gallery') }}">Gallery</a></li>
+                            <li aria-current="page" class="breadcrumb-item active">{{ $galleryAlbum->title }}</li>
                         </ul>
                     </nav>
                 </div>
@@ -20,153 +21,44 @@
         <div class="dot-overlay"></div>
     </section>
     <!-- BreadCrumb Ends -->
+
     <!-- Gallery starts -->
     <div class="gallery pt-10 pb-6">
         <div class="container">
             <div class="row mt-3">
-                <div class="col-md-6">
-                    <div class="gallery-item mb-4">
-                        <div class="gallery-image">
-                            <img alt="image" src="{{ asset('template/yatri_world/main-file/images/tibet.jpg') }}" />
-                            <div class="overlay"></div>
-                        </div>
-                        <div class="gallery-content">
-                            <ul>
-                                <li><a data-lightbox="gallery" data-title="Title" href="images/trending/trending1.jpg"><i
-                                            class="fa fa-eye"></i></a></li>
-                                <li><a href="#"><i class="fa fa-link"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="gallery-item mb-4">
-                        <div class="gallery-image">
-                            <img alt="image" src="{{ asset('template/yatri_world/main-file/images/tibet.jpg') }}" />
-                            <div class="overlay"></div>
-                        </div>
-                        <div class="gallery-content">
-                            <ul>
-                                <li><a data-lightbox="gallery" data-title="Title" href="images/trending/trending2.jpg"><i
-                                            class="fa fa-eye"></i></a></li>
-                                <li><a href="#"><i class="fa fa-link"></i></a></li>
-                            </ul>
+                @forelse($galleryAlbum->galleryMedia as $media)
+                    <div class="col-lg-4 col-md-6">
+                        <div class="gallery-item mb-4">
+                            <div class="gallery-image">
+                                <img alt="image" src="{{ $media->image }}" />
+                                <div class="overlay"></div>
+                            </div>
+                            <div class="gallery-content">
+                                <ul>
+                                    <li>
+                                        <a data-lightbox="gallery" data-title="{{ $galleryAlbum->title }}"
+                                            href="{{ $media->image }}">
+                                            <i class="fa fa-eye"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ $media->url ?? '#' }}"><i class="fa fa-link"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="gallery-item mb-4">
-                        <div class="gallery-image">
-                            <img alt="image" src="{{ asset('template/yatri_world/main-file/images/everest.jpg') }}" />
-                            <div class="overlay"></div>
-                        </div>
-                        <div class="gallery-content">
-                            <ul>
-                                <li><a data-lightbox="gallery" data-title="Title" href="images/trending/trending3.jpg"><i
-                                            class="fa fa-eye"></i></a></li>
-                                <li><a href="#"><i class="fa fa-link"></i></a></li>
-                            </ul>
-                        </div>
+                @empty
+                    <div class="col-12 text-center">
+                        <p>No media found in this album.</p>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="gallery-item mb-4">
-                        <div class="gallery-image">
-                            <img alt="image" src="{{ asset('template/yatri_world/main-file/images/bhutan.jpg') }}" />
-                            <div class="overlay"></div>
-                        </div>
-                        <div class="gallery-content">
-                            <ul>
-                                <li><a data-lightbox="gallery" data-title="Title" href="images/trending/trending4.jpg"><i
-                                            class="fa fa-eye"></i></a></li>
-                                <li><a href="#"><i class="fa fa-link"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="gallery-item mb-4">
-                        <div class="gallery-image">
-                            <img alt="image" src="{{ asset('template/yatri_world/main-file/images/india.jpg') }}" />
-                            <div class="overlay"></div>
-                        </div>
-                        <div class="gallery-content">
-                            <ul>
-                                <li><a data-lightbox="gallery" data-title="Title" href="images/trending/trending5.jpg"><i
-                                            class="fa fa-eye"></i></a></li>
-                                <li><a href="#"><i class="fa fa-link"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="gallery-item mb-4">
-                        <div class="gallery-image">
-                            <img alt="image" src="{{ asset('template/yatri_world/main-file/images/bhutan.jpg') }}" />
-                            <div class="overlay"></div>
-                        </div>
-                        <div class="gallery-content">
-                            <ul>
-                                <li><a data-lightbox="gallery" data-title="Title" href="images/trending/trending6.jpg"><i
-                                            class="fa fa-eye"></i></a></li>
-                                <li><a href="#"><i class="fa fa-link"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="gallery-item mb-4">
-                        <div class="gallery-image">
-                            <img alt="image" src="{{ asset('template/yatri_world/main-file/images/tibet.jpg') }}" />
-                            <div class="overlay"></div>
-                        </div>
-                        <div class="gallery-content">
-                            <ul>
-                                <li><a data-lightbox="gallery" data-title="Title" href="images/trending/trending7.jpg"><i
-                                            class="fa fa-eye"></i></a></li>
-                                <li><a href="#"><i class="fa fa-link"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="gallery-item mb-4">
-                        <div class="gallery-image">
-                            <img alt="image" src="{{ asset('template/yatri_world/main-file/images/tibet.jpg') }}" />
-                            <div class="overlay"></div>
-                        </div>
-                        <div class="gallery-content">
-                            <ul>
-                                <li><a data-lightbox="gallery" data-title="Title" href="images/trending/trending8.jpg"><i
-                                            class="fa fa-eye"></i></a></li>
-                                <li><a href="#"><i class="fa fa-link"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="col-lg-12">
-                    <div class="pagination-main text-center">
-                        <ul class="pagination">
-                            <li><a href="#"><i aria-hidden="true" class="fa fa-angle-double-left"></i></a>
-                            </li>
-                            <li class="active"><a href="#">1</a></li>
-                            <li><a href="#">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">4</a></li>
-                            <li><a href="#"><i aria-hidden="true" class="fa fa-angle-double-right"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-
+                @endforelse
             </div>
         </div>
     </div>
     <!-- Gallery Ends -->
 @endsection
+
 @push('styles')
     <style>
         .gallery-image img {
