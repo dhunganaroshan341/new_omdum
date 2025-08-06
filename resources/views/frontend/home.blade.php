10d940fb3a454d146741bd7dbcaf256cf9f81e2c
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-06 17:27:06 +0545

    Auto commit changes

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Fav destination Starts -->
 
-    <x-fav-destination />
+    {{-- <x-fav-destination /> --}}
     <!-- Fav destination Ends -->
 
 
@@ -78,7 +78,7 @@
     <!-- call to action Ends -->
 
     <!-- top destination starts -->
-<x-random-packages/>
+    <x-random-packages />
 
 
 
