581965a8274fb63e7c9d7e747b79dfae0503b812
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-07 14:46:58 +0545

    Auto commit changes

--- resources/views/frontend/packages-grid.blade.php
+++ resources/views/frontend/packages-grid.blade.php
@@ -32,8 +32,12 @@
                                 <div class="list-results d-flex align-items-center justify-content-between">
                                     <div class="list-results-sort">
                                         <p class="m-0">
-                                            Showing {{ $tourPackages->firstItem() }}–{{ $tourPackages->lastItem() }} of
-                                            {{ $tourPackages->total() }} results
+                                            @if ($tourPackages->count == 0)
+                                                Packages Not Available
+                                            @else
+                                                Showing {{ $tourPackages->firstItem() }}–{{ $tourPackages->lastItem() }} of
+                                                {{ $tourPackages->total() }} results
+                                            @endif
                                         </p>
                                     </div>
 
