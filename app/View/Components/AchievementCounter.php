fa53ede94988e512399fbf709804282096eaa308
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-08 16:02:45 +0545

    Auto commit changes

--- app/View/Components/AchievementCounter.php
+++ app/View/Components/AchievementCounter.php
@@ -13,17 +13,18 @@ class AchievementCounter extends Component
 
     public function __construct()
     {
-        $achievements = Achievement::take(4)->get();
-
+        // $achievements = Achievement::take(4)->get();
+        //  $achievements =null;
+// $achievents;
         // Optional fallback logic if not enough achievements
-        if ($achievements->isEmpty()) {
+        // if ($achievements->isEmpty()) {
             $achievements = collect([
                 (object)[ 'count' => 1000, 'title' => 'Happy Customers', 'icon_class' => 'fa-smile' ],
                 (object)[ 'count' => 200, 'title' => 'Amazing Tours', 'icon_class' => 'fa-plane' ],
                 (object)[ 'count' => 20, 'title' => 'Years In Business', 'icon_class' => 'fa-clock' ],
                 (object)[ 'count' => 523, 'title' => 'Support Case', 'icon_class' => 'fa-headset' ],
             ]);
-        }
+        // }
 
         $this->achievements = $achievements;
     }
