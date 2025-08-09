b1f9e38f45aafb9a4174b24522285b9cefa6fec9
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-09 15:47:21 +0545

    Auto commit changes

--- resources/views/Admin/pages/bookings/index.blade.php
+++ resources/views/Admin/pages/bookings/index.blade.php
@@ -1,35 +1,43 @@
-<div class="row mb-3">
-    <div class="col-md-3">
-        <label for="startDate" class="form-label fw-semibold">Start Date</label>
-        <input type="date" id="startDate" class="form-control" />
-    </div>
-    <div class="col-md-3">
-        <label for="endDate" class="form-label fw-semibold">End Date</label>
-        <input type="date" id="endDate" class="form-control" />
-    </div>
-    <div class="col-md-6 d-flex align-items-end">
-        <button id="filterBtn" class="btn btn-primary me-2">Filter</button>
-        <button id="resetBtn" class="btn btn-secondary">Reset</button>
-    </div>
-</div>
+@extends('Admin.layout.master')
 
-<div class="table-responsive">
-    <table class="table table-striped" id="show-booking-data">
-        <thead>
-            <tr>
-                <th>S.N</th>
-                <th>Action</th>
-                <th>Package</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Booking Type</th>
-                <th>Booking Date</th>
-                <th>No. of People</th>
-                <th>Price</th>
-                <th>Status</th>
-                <th>Submitted At</th>
-            </tr>
-        </thead>
-    </table>
-</div>
+@section('content')
+    <div class="container-fluid">
+
+        <div class="row mb-3">
+            <div class="col-md-3">
+                <label for="startDate" class="form-label fw-semibold">Start Date</label>
+                <input type="date" id="startDate" class="form-control" />
+            </div>
+            <div class="col-md-3">
+                <label for="endDate" class="form-label fw-semibold">End Date</label>
+                <input type="date" id="endDate" class="form-control" />
+            </div>
+            <div class="col-md-6 d-flex align-items-end">
+                <button id="filterBtn" class="btn btn-primary me-2">Filter</button>
+                <button id="resetBtn" class="btn btn-secondary">Reset</button>
+            </div>
+        </div>
+
+        <div class="table-responsive">
+            <table class="table table-striped" id="show-booking-data">
+                <thead>
+                    <tr>
+                        <th>S.N</th>
+                        <th>Action</th>
+                        <th>Package</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Booking Type</th>
+                        <th>Booking Date</th>
+                        <th>No. of People</th>
+                        <th>Price</th>
+                        <th>Status</th>
+                        <th>Submitted At</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+
+    </div>
+@endsection
