1c9d265885a81dc0aa9d709a2e98c9e9943d1ebc
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-09 15:45:33 +0545

    Auto commit changes

--- resources/views/Admin/pages/bookings/index.blade.php
+++ resources/views/Admin/pages/bookings/index.blade.php
@@ -1,29 +1,35 @@
-@extends('Admin.layout.master')
-@section('content')
-    <div class="container-fluid">
-        <button class="btn btn-primary addCallToActionBtn mb-4 mt-4">Bookings</button>
-        @include('Admin.pages.CallToAction.callToActionModal')
-
-        <div class="table-responsive">
-            <table class="table table-striped" id="show-booking-data">
-                <thead>
-                    <tr>
-                        <th>S.N</th>
-                        <th>Action</th>
-                        <th>Package</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Booking Type</th>
-                        <th>Booking Date</th>
-                        <th>No. of People</th>
-                        <th>Price</th>
-                        <th>Status</th>
-                        <th>Submitted At</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
-
+<div class="row mb-3">
+    <div class="col-md-3">
+        <label for="startDate" class="form-label fw-semibold">Start Date</label>
+        <input type="date" id="startDate" class="form-control" />
     </div>
-@endsection
+    <div class="col-md-3">
+        <label for="endDate" class="form-label fw-semibold">End Date</label>
+        <input type="date" id="endDate" class="form-control" />
+    </div>
+    <div class="col-md-6 d-flex align-items-end">
+        <button id="filterBtn" class="btn btn-primary me-2">Filter</button>
+        <button id="resetBtn" class="btn btn-secondary">Reset</button>
+    </div>
+</div>
+
+<div class="table-responsive">
+    <table class="table table-striped" id="show-booking-data">
+        <thead>
+            <tr>
+                <th>S.N</th>
+                <th>Action</th>
+                <th>Package</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Booking Type</th>
+                <th>Booking Date</th>
+                <th>No. of People</th>
+                <th>Price</th>
+                <th>Status</th>
+                <th>Submitted At</th>
+            </tr>
+        </thead>
+    </table>
+</div>
