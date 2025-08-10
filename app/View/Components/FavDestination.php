eb8659bdb57a94010c8f01dc395a9263b6806742
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-10 14:52:12 +0545

    Auto commit changes

--- app/View/Components/FavDestination.php
+++ app/View/Components/FavDestination.php
@@ -12,15 +12,26 @@ class FavDestination extends Component
     public $favouriteDestinationsByCountry;
 
     public function __construct()
-    {
-        $countries = OurCountry::with(['favouritePackages' => function ($query) {
-            $query->with(['images', 'country']);
-        }])->get();
+{
+    // Define your custom order by slug (lowercase)
+    $customOrder = ['nepal', 'india', 'tibet', 'bhutan'];
 
-        $this->favouriteDestinationsByCountry = $countries->filter(
-            fn ($country) => $country->favouritePackages->isNotEmpty()
-        );
-    }
+    // Fetch countries with favouritePackages and eager loading
+    $countries = OurCountry::with(['favouritePackages' => function ($query) {
+        $query->with(['images', 'country']);
+    }])->get();
+
+    // Filter countries which have favouritePackages
+    $filteredCountries = $countries->filter(fn($country) => $country->favouritePackages->isNotEmpty());
+
+    // Sort with Nepal first, then India, Tibet, Bhutan, and others after
+    $this->favouriteDestinationsByCountry = $filteredCountries->sortBy(function ($country) use ($customOrder) {
+        $slug = strtolower($country->slug);
+        $index = array_search($slug, $customOrder);
+        return $index === false ? 999 : $index;
+    })->values(); // reset keys
+}
+
 
     public function render(): View|Closure|string
     {
