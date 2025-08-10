64fc7213403d8954eb49a59c0e42c559388dc55e
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-10 16:24:37 +0545

    Auto commit changes

--- resources/views/Admin/layout/header.blade.php
+++ resources/views/Admin/layout/header.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title> Omundum | Admin Panel </title>
+    <title> {{ $title ?? 'Omundum' }} | Admin Panel </title>
 
 
     <!-- jQuery -->
