eeb7a624c0e0e1bc88cb6fe40eea8549f3ffd1ac
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-11 16:37:51 +0545

    Auto commit changes

--- resources/views/frontend/packages-grid.blade.php
+++ resources/views/frontend/packages-grid.blade.php
@@ -340,7 +340,7 @@
                                 <div
                                     class="desti-content position-absolute bottom-0 start-0 w-100 p-3 bg-navy bg-opacity-75">
                                     <h4 class="white mb-1">
-                                        <a href="{{ route('packages.show', ['id' => $package->id]) }}"
+                                        <a href="{{ route('packages.show', ['slug' => $package->slug]) }}"
                                             class="text-decoration-none white">
                                             {{ $package->title ?? 'Mundum Cultural Trek' }}
                                         </a>
@@ -361,7 +361,7 @@
 
                                 <div
                                     class="desti-overlay position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center bg-dark bg-opacity-50">
-                                    <a class="nir-btn" href="{{ route('packages.show', ['id' => $package->id]) }}">
+                                    <a class="nir-btn" href="{{ route('packages.show', ['slug' => $package->slug]) }}">
                                         <span class="white boonow-span">Book Now</span>
                                         <i class="fa fa-arrow-right white ps-1 book-now-i"></i>
                                     </a>
@@ -453,12 +453,12 @@
         /* Change all key text to green on hover */
         /* Beat .white class color with higher specificity */
         /* .desti-image:hover .desti-content h4 a.white,
-                                                                                                                                                                                                                                                .desti-image:hover .trend-last-main p.white,
-                                                                                                                                                                                                                                                .desti-image:hover .trend-last-main .price span,
-                                                                                                                                                                                                                                                .desti-image:hover .desti-overlay a span.white,
-                                                                                                                                                                                                                                                .desti-image:hover .desti-overlay a i.white {
-                                                                                                                                                                                                                                                    color: var(--omundum-green) !important;
-                                                                                                                                                                                                                                                } */
+                                                                                                                                                                                                                                                    .desti-image:hover .trend-last-main p.white,
+                                                                                                                                                                                                                                                    .desti-image:hover .trend-last-main .price span,
+                                                                                                                                                                                                                                                    .desti-image:hover .desti-overlay a span.white,
+                                                                                                                                                                                                                                                    .desti-image:hover .desti-overlay a i.white {
+                                                                                                                                                                                                                                                        color: var(--omundum-green) !important;
+                                                                                                                                                                                                                                                    } */
     </style>
 @endpush
 @push('scripts')
