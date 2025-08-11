da91b525d1389818e8dafddea2778826da777bc6
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-11 15:06:39 +0545

    Auto commit changes

--- resources/views/Admin/pages/TourPackage/tourPackage.blade.php
+++ resources/views/Admin/pages/TourPackage/tourPackage.blade.php
@@ -14,21 +14,19 @@
                         <th scope="col" class="text-center">Status</th>
                         <th scope="col">Action</th>
                         <th scope="col">Package</th>
-                        {{-- <th scope="col">Head Package</th> --}}
-                        {{-- <th scope="col">Fav</th> --}}
+                        <!-- <th scope="col">Head Package</th> -->
+                        <!-- <th scope="col">Fav</th> -->
                         <th scope="col">Duration</th>
                         <th scope="col">Country</th>
                         <th scope="col">Top Deal</th>
                         <th scope="col">Fav Destination</th>
                         <th scope="col">Images</th>
-
                         <th scope="col">Itinerary</th>
                         <th scope="col">Batches</th>
                         <th scope="col">Package Includes</th>
                     </tr>
-
-
                 </thead>
+
 
             </table>
         </div>
