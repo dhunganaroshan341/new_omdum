61706b1b9938daad1f30d08dc8c6d4f74fce2696
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-12 14:05:59 +0545

    Auto commit changes

--- resources/views/components/client-carousel.blade.php
+++ resources/views/components/client-carousel.blade.php
@@ -6,13 +6,13 @@
 
         <div class="attract-slider owl-carousel">
             @dd($clients);
-            {{-- @foreach ($clients as $client) --}}
-            <div class="client-logo item">
-                @dd($client->image_url);
+            @foreach ($clients as $client)
+                <div class="client-logo item">
+                    @dd($client->image_url);
 
-                <img src="{{ $client->image_url ?? asset('template/yatri_world/main-file/images/clients/logo-01.png') }}"
-                    alt="client">
-            </div>
+                    <img src="{{ $client->image_url ?? asset('template/yatri_world/main-file/images/clients/logo-01.png') }}"
+                        alt="client">
+                </div>
             @endforeach
         </div>
     </div>
