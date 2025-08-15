06f7ce429b59a8c1591ac684b8a870fd4f2359fc
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-15 12:30:53 +0545

    Auto commit changes

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TeamFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name'     => $this->faker->name(),
+            'position' => $this->faker->jobTitle(),
+            'photo'    => 'https://i.pravatar.cc/300?img=' . $this->faker->numberBetween(1, 70),
+            'facebook' => $this->faker->url(),
+            'twitter'  => $this->faker->url(),
+            'linkedin' => $this->faker->url(),
+        ];
+    }
+}
