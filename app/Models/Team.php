7e1cca35a117eb800b1bd14ef0cd7c050b0f980a
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-15 12:33:20 +0545

    Auto commit changes

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+
+
 
 class Team extends Model
 {
