0ceca5aa79017c79c310847b8c3ac5fda281c046
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-29 22:44:05 +0545

    Auto commit changes

--- resources/views/components/fav-destination.blade.php
+++ resources/views/components/fav-destination.blade.php
@@ -50,7 +50,7 @@
                                                             href="{{ $routeUrl }}">{{ $package->title ?? 'Untitled Package' }}</a>
                                                     </h4>
                                                     <span class="omundum-text-green fw-bold">
-                                                        ${{ $package->price ?? '' }}
+                                                        {{ $package->price ? '$' . $package->price : '' }}
                                                     </span>
                                                 </div>
 
