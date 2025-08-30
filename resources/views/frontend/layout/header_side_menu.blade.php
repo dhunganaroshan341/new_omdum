512dc3cc28b6fb80f13c88fa87c3c2436c13b340
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-30 08:39:07 +0545

    Auto commit changes

---
+++ resources/views/frontend/layout/header_side_menu.blade.php
@@ -0,0 +1,81 @@
+   <!-- header side menu -->
+   <div class="header_sidemenu">
+       <div class="header_sidemenu_in">
+           <div class="menu">
+               <div class="close-menu">
+                   <i class="fa fa-times white"></i>
+               </div>
+               <div class="m-contentmain">
+                   <div class="cart-main">
+                       <div class="cart-box">
+                           <div class="popup-container">
+                               <h5 class="p-3 mb-0 bg-pink white text-caps">My Carts(3 Items)</h5>
+                               <div class="cart-entry d-flex align-items-center p-3">
+                                   <a href="#" class="image">
+                                       <img src="images/shop/shop1.jpg" alt="">
+                                   </a>
+                                   <div class="content">
+                                       <a href="#" class="title font-weight-bold">Pullover Batwing</a>
+                                       <p class="quantity m-0">Quantity: 3</p>
+                                       <span class="price">$45.00</span>
+                                   </div>
+                                   <div class="button-x">
+                                       <i class="icon-close"></i>
+                                   </div>
+                               </div>
+                               <div class="cart-entry d-flex align-items-center p-3">
+                                   <a href="#" class="image">
+                                       <img src="images/shop/shop2.jpg" alt="">
+                                   </a>
+                                   <div class="content">
+                                       <a href="#" class="title font-weight-bold">Pullover Batwing</a>
+                                       <p class="quantity m-0">Quantity: 3</p>
+                                       <span class="price">$90.00</span>
+                                   </div>
+                                   <div class="button-x">
+                                       <i class="icon-close"></i>
+                                   </div>
+                               </div>
+                               <div class="cart-entry d-flex align-items-center p-3">
+                                   <a href="#" class="image">
+                                       <img src="images/shop/shop6.jpg" alt="">
+                                   </a>
+                                   <div class="content">
+                                       <a href="#" class="title font-weight-bold">Pullover Batwing</a>
+                                       <p class="quantity m-0">Quantity: 3</p>
+                                       <span class="price">$90.00</span>
+                                   </div>
+                                   <div class="button-x">
+                                       <i class="icon-close"></i>
+                                   </div>
+                               </div>
+                               <div class="summary-total">
+                                   <div class="summary d-flex align-items-center justify-content-between">
+                                       <div class="subtotal font-weight-bold">Delivery Charge</div>
+                                       <div class="price-s">$10</div>
+                                   </div>
+                                   <div class="summary d-flex align-items-center justify-content-between">
+                                       <div class="subtotal font-weight-bold">Sub Total</div>
+                                       <div class="price-s">$200</div>
+                                   </div>
+                                   <div class="summary d-flex align-items-center justify-content-between">
+                                       <div class="subtotal font-weight-bold">Discount</div>
+                                       <div class="price-s">$2</div>
+                                   </div>
+                                   <div class="summary d-flex align-items-center justify-content-between">
+                                       <div class="subtotal font-weight-bold">Total</div>
+                                       <div class="price-s">$208</div>
+                                   </div>
+                               </div>
+                               <div class="cart-buttons d-flex align-items-center justify-content-between">
+                                   <a href="#" class="nir-btn">View Cart</a>
+                                   <a href="#" class="nir-btn-black">Checkout</a>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <div class="overlay hide"></div>
+       </div>
+   </div>
