0183a22ef34371d5a28e7f6d33ea95fa82db3fb0
Roshan Dhungana <79978683+dhunganaroshan341@users.noreply.github.com>
2026-05-27 16:49:27 +0545

    Delete resources/views directory

--- resources/views/emails/contact.blade.php
+++
@@ -1,5 +0,0 @@
-<h2>Contact Message From Omundum Treks</h2>
-<p><strong>Name:</strong> {{ $formData['name'] ?? 'N/A' }}</p>
-<p><strong>Email:</strong> {{ $formData['email'] ?? 'N/A' }}</p>
-<p><strong>Phone:</strong> {{ $formData['phone'] ?? '' }}</p>
-<p><strong>Message:</strong> {{ $formData['message'] ?? 'N/A' }}</p>
